5ebc44859b20f2009b2ac4180f2cd39bdc2321e8
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-05-09 22:34:39 -0400

    add wine

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-var express = require('express');
-var server = express();
+var express = require('express'),
+    wine = require('./routes/wines'),
+    app = express();
 
-server.configure(function(){
-  server.use(express.static(__dirname + '/app'));
+app.configure(function() {
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/app'));
 });
 
-server.listen(3000);
+app.get('/wines', wine.findAll);
+app.get('/wines/:id', wine.findById);
+app.post('/wines', wine.addWine);
+app.put('/wines/:id', wine.updateWine);
+app.delete('/wines/:id', wine.deleteWine);
+
+app.listen(3000);
+console.log('Listening on port 3000...');
