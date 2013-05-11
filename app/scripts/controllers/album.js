374035e126562a92d93ecf886e5eb0f58485e077
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-05-11 12:28:51 -0400

    new

---
+++ app/scripts/controllers/album.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularMeetupApp')
+  .controller('AlbumCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/member.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularMeetupApp')
+  .controller('MemberCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
