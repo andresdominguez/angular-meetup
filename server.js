4d7455d85099c789fc4398a7244103ef870241ee
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-05-11 13:59:19 -0400

    add the whole crud

--- server.js
+++ server.js
@@ -10,15 +10,21 @@ app.configure(function() {
 
 app.get('/bands', bandService.findAllBands);
 app.get('/bands/:id', bandService.findBandById);
-//app.post('/bands', bandService.addWine);
-//app.put('/bands/:id', bandService.updateWine);
-//app.delete('/bands/:id', bandService.deleteWine);
+app.post('/bands', bandService.createBand);
+app.put('/bands/:id', bandService.updateBand);
+app.delete('/bands/:id', bandService.deleteBand);
 
 app.get('/members', bandService.findAllMembers);
 app.get('/members/:id', bandService.findMemberById);
+app.post('/members', bandService.createMember);
+app.put('/members/:id', bandService.updateMember);
+app.delete('/members/:id', bandService.deleteMember);
 
 app.get('/albums', bandService.findAllAlbums);
 app.get('/albums/:id', bandService.findAlbumById);
+app.post('/albums', bandService.createAlbum);
+app.put('/albums/:id', bandService.updateAlbum);
+app.delete('/albums/:id', bandService.deleteAlbum);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
