e6187ef9a5d4e4508130bcb0d3c238c7699415be
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-05-11 15:08:19 -0400

    work in progress

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,23 +1,12 @@
 'use strict';
 
-var API_PATH = 'http://localhost:3000/';
+//angular.module('angularMeetupApp', ['ngResource']);
 
-var app = angular.module('angularMeetupApp');
+angular.module('angularMeetupApp').
+    controller('MainCtrl', function($scope, apiService) {
 
-app.controller('MainCtrl', function($scope, $http) {
-  $http.get(API_PATH + 'wines').success(function(data) {
-    $scope.awesomeThings = data;
-  });
-});
-
-app.controller('ViewIngredientCtrl', function($scope, $routeParams, $http) {
-  var url = API_PATH + 'wines/' + $routeParams.id;
-  $http.get(url).success(function(data) {
-    $scope.item = data;
-  });
-});
-
-/*
- * Band has albums
- * Band has members
- * */
+      apiService.band.query(function(data) {
+        debugger;
+        $scope.bandList = data;
+      });
+    });
