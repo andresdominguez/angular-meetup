bb7cd6f0376fed709f33eca9e70ebcbf14ae5f76
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-05-11 13:32:51 -0400

    work in progress

--- routes/band-server.js
+++ routes/band-server.js
@@ -4,19 +4,21 @@ var _ = require("underscore");
 var Server = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure,
-    WINES = 'wines';
+    BANDS = 'bands',
+    MEMBERS = 'members',
+    ALBUMS = 'albums'
 
 var menuApp = {};
 
 var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('winedb', server);
+db = new Db('banddb', server);
 
 db.open(function(err, db) {
   if (!err) {
-    console.log("Connected to 'winedb' database");
+    console.log("Connected to 'banddb' database");
     db.collection('wines', {strict: true}, function(err, collection) {
       if (err) {
-        console.log("The 'wines' collection doesn't exist. Creating it with sample data...");
+        console.log("The collection does not exist, creating a new one");
         populateDB();
       }
     });
@@ -42,12 +44,12 @@ exports.findById = function(req, res) {
   });
 };
 
-exports.findAll = _.partial(menuApp.findAll, WINES);
+exports.findAll = _.partial(menuApp.findAll, BANDS);
 
 exports.addWine = function(req, res) {
   var wine = req.body;
   console.log('Adding wine: ' + JSON.stringify(wine));
-  db.collection(WINES, function(err, collection) {
+  db.collection(BANDS, function(err, collection) {
     collection.insert(wine, {safe: true}, function(err, result) {
       if (err) {
         res.send({'error': 'An error has occurred'});
@@ -64,7 +66,7 @@ exports.updateWine = function(req, res) {
   var wine = req.body;
   console.log('Updating wine: ' + id);
   console.log(JSON.stringify(wine));
-  db.collection(WINES, function(err, collection) {
+  db.collection(BANDS, function(err, collection) {
     collection.update({'_id': new BSON.ObjectID(id)}, wine, {safe: true}, function(err, result) {
       if (err) {
         console.log('Error updating wine: ' + err);
@@ -80,7 +82,7 @@ exports.updateWine = function(req, res) {
 exports.deleteWine = function(req, res) {
   var id = req.params.id;
   console.log('Deleting wine: ' + id);
-  db.collection(WINES, function(err, collection) {
+  db.collection(BANDS, function(err, collection) {
     collection.remove({'_id': new BSON.ObjectID(id)}, {safe: true}, function(err, result) {
       if (err) {
         res.send({'error': 'An error has occurred - ' + err});
@@ -92,33 +94,45 @@ exports.deleteWine = function(req, res) {
   });
 }
 
-/*--------------------------------------------------------------------------------------------------------------------*/
-// Populate database with sample data -- Only used once: the first time the application is started.
-// You'd typically not find this code in a real-life app, since the database would already exist.
 var populateDB = function() {
-
-  var wines = [
+  var bands = [
     {
-      name: "CHATEAU DE SAINT COSME",
-      year: "2009",
-      grapes: "Grenache / Syrah",
-      country: "France",
-      region: "Southern Rhone",
-      description: "The aromas of fruit and spice...",
-      picture: "saint_cosme.jpg"
+      name: 'Wu-Tang Clan',
+      albums: [],
+      members: []
     },
     {
-      name: "LAN RIOJA CRIANZA",
-      year: "2006",
-      grapes: "Tempranillo",
-      country: "Spain",
-      region: "Rioja",
-      description: "A resurgence of interest in boutique vineyards...",
-      picture: "lan_rioja.jpg"
-    }];
+      name: 'The Police',
+      albums: [],
+      members: []
+    }
+  ];
 
-  db.collection(WINES, function(err, collection) {
-    collection.insert(wines, {safe:true}, function(err, result) {});
+  var members = [
+    {
+      name: 'Sting'
+    },
+    {
+      name: 'Method Man'
+    },
+    {
+      name: 'Ghostface Killah'
+    }
+  ];
+
+  var albums = [
+      {
+        name: 'Synchronicity'
+      }
+  ];
+
+  db.collection(BANDS, function(err, collection) {
+    collection.insert(bands, {safe:true}, function(err, result) {});
   });
-
+  db.collection(ALBUMS, function(err, collection) {
+    collection.insert(albums, {safe:true}, function(err, result) {});
+  });
+  db.collection(MEMBERS, function(err, collection) {
+    collection.insert(members, {safe:true}, function(err, result) {});
+  });
 };
