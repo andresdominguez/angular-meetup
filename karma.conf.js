bc2218776138199b0afc2192e70e813245d92c97
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-05-12 13:29:51 -0400

    add angular resource

--- karma.conf.js
+++ karma.conf.js
@@ -8,6 +8,7 @@ files = [
   JASMINE,
   JASMINE_ADAPTER,
   'app/components/angular/angular.js',
+  'app/components/angular-resource/angular-resource.js',
   'app/components/angular-mocks/angular-mocks.js',
   'app/scripts/*.js',
   'app/scripts/**/*.js',
