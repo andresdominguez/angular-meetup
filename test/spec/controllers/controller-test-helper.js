26de8780ae7f38fc5b040fb75f094770e4965a29
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-05-12 17:27:47 -0400

    small refactoring

--- test/spec/controllers/controller-test-helper.js
+++ test/spec/controllers/controller-test-helper.js
@@ -3,28 +3,57 @@ var testHelper = angular.module('ControllerTestHelper', ['angularMeetupApp']);
 testHelper.service('fakeResource', function(apiService, _$httpBackend_) {
   var h = _$httpBackend_;
 
-  var members = [
-    {name: 'Method Man'},
-    {name: 'Ghostface Killah'}
-  ];
-
   var addReturns = function(requestHandler, response) {
-    requestHandler.respond(response)
+    requestHandler.respond(_.isFunction(response) ? response() : response);
   };
 
-  var addWhenGet = function(url, response) {
+  var addWhenGet = function(url, defaultResponse) {
     var requestHandler = h.whenGET(url);
 
     return {
       returnsDefault: function() {
+        addReturns(requestHandler, defaultResponse)
+      },
+      returns: function(response) {
         addReturns(requestHandler, response)
       }
     }
   };
 
-  var member = {
-    whenGetList: _.partial(addWhenGet, '/members', members)
-  }
+  var createResource = function(settings) {
+    return {
+      whenGetById: _.partial(addWhenGet,
+          settings.list.url, settings.list.response),
+      whenGetList: _.partial(addWhenGet,
+          settings.list.url, settings.list.response)
+    }
+  };
+
+  var mock = {
+    member: {
+      getById: function() {
+        return {name: 'Method Man'}
+      },
+      getList: function() {
+        return [
+          {name: 'Method Man'},
+          {name: 'Ghostface Killah'}
+        ];
+      }
+    }
+  };
+
+  var member = createResource({
+    byId: {
+      url: new RegExp('/members/[0-9]+'),
+      response: mock.member.getById()
+    },
+    list: {
+      url: '/members',
+      response: mock.member.getList()
+    }
+  });
+
 
   return {
     member: member
