b7ce46cc1db7927ec8e5ec0840c2b5f4b860e4e1
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-05-12 12:04:04 -0400

    add members

--- app/scripts/controllers/band-edit.js
+++ app/scripts/controllers/band-edit.js
@@ -4,6 +4,10 @@ angular.module('angularMeetupApp').controller('BandEditCtrl',
     function($scope, $routeParams, apiService) {
       var bandId = $routeParams.bandId;
 
+      $scope.addMember = function() {
+        $scope.item.members.push($scope.selectedMember);
+      };
+
       apiService.band.get({id: bandId}, function(data) {
         $scope.item = data;
       });
