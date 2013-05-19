3cf4abb83b48f12c083bab66e3ed70d9667e40c6
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-05-19 13:06:43 -0400

    make the test work

--- test/spec/controllers/member-list-spec.js
+++ test/spec/controllers/member-list-spec.js
@@ -1,22 +1,38 @@
 'use strict';
 
-describe('Controller: MemberListCtrl', function () {
+ddescribe('Controller: MemberListCtrl', function() {
 
-  // load the controller's module
-  beforeEach(module('angularMeetupApp'));
+  beforeEach(module('angularMeetupApp', 'ControllerTestHelper'));
 
-  var MemberListCtrl,
-    scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MemberListCtrl = $controller('MemberListCtrl', {
-      $scope: scope
-    });
+  beforeEach(inject(function(jasmineMatchers) {
+    this.addMatchers(jasmineMatchers);
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  var MemberListCtrl, scope, createController, fake;
+
+  beforeEach(inject(function($controller, $rootScope, fakeResource) {
+    fake = fakeResource;
+
+    createController = function() {
+      scope = $rootScope.$new();
+      MemberListCtrl = $controller('MemberListCtrl', {
+        $scope: scope
+      });
+    };
+  }));
+
+  it('should get a list of members', function() {
+    // Expect the controller to fetch a list of members.
+    fake.member.whenGetList().returnsDefault();
+
+    // When you create the controller.
+    createController();
+
+    // Nothing yet because the response has not arrived.
+    expect(scope.list).toBeUndefined();
+    fake.flush();
+
+    // Then ensure the member list got populated.
+    expect(scope.list.length).toBe(3);
   });
 });
