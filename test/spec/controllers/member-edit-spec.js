1d5497273395cec90a8314dd95fcf2b140096d6d
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-05-19 13:21:01 -0400

    add unit tests

--- test/spec/controllers/member-edit-spec.js
+++ test/spec/controllers/member-edit-spec.js
@@ -1,22 +1,56 @@
 'use strict';
 
-describe('Controller: MemberEditCtrl', function () {
+ddescribe('Controller: MemberEditCtrl', function() {
 
-  // load the controller's module
-  beforeEach(module('angularMeetupApp'));
+  beforeEach(module('angularMeetupApp', 'ControllerTestHelper'));
 
-  var MemberEditCtrl,
-    scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MemberEditCtrl = $controller('MemberEditCtrl', {
-      $scope: scope
-    });
+  beforeEach(inject(function(jasmineMatchers) {
+    this.addMatchers(jasmineMatchers);
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  var MemberEditCtrl, scope, createController, fake;
+
+  beforeEach(inject(function($controller, $rootScope, $routeParams, fakeResource) {
+    fake = fakeResource;
+
+    createController = function(memberId) {
+      $routeParams.memberId = memberId;
+
+      scope = $rootScope.$new();
+      MemberEditCtrl = $controller('MemberEditCtrl', {
+        $scope: scope
+      });
+    };
+  }));
+
+
+  it('should create a new member', function() {
+    fake.member.whenCreate().returns({id: 1});
+
+    // Given that you load the controller to create a new member.
+    createController('new');
+
+    // When you save.
+    scope.save();
+
+    // Then ensure a new member was created.
+    expect(fake.member).toHaveBeenCreated();
+  });
+
+  it('should update an existing member', function() {
+    fake.member.whenGetById().returnsDefault();
+    fake.member.whenUpdate().returns();
+
+    // Given that load an existing member.
+    createController(321);
+    fake.flush();
+
+    // When you save.
+    scope.item.name = 'updated name';
+    scope.save();
+
+    // When ensure the member was updated.
+    expect(fake.member).toHaveBeenRequested();
+    expect(fake.member).toHaveBeenUpdated({name: 'updated name'});
   });
 });
