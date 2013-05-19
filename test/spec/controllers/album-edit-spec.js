759571eee73c644c19cef4b5ff354b87fe2e3145
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-05-19 13:21:43 -0400

    dd

--- test/spec/controllers/album-edit-spec.js
+++ test/spec/controllers/album-edit-spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-ddescribe('Controller: AlbumEditCtrl', function() {
+describe('Controller: AlbumEditCtrl', function() {
 
   beforeEach(module('angularMeetupApp', 'ControllerTestHelper'));
 
