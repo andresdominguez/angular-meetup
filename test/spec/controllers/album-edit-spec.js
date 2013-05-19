eb50e4697101ce01ef7babf1721b8ec1e3df1f5e
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-05-19 12:52:05 -0400

    add tests

--- test/spec/controllers/album-edit-spec.js
+++ test/spec/controllers/album-edit-spec.js
@@ -1,22 +1,47 @@
 'use strict';
 
-describe('Controller: AlbumEditCtrl', function () {
+ddescribe('Controller: AlbumEditCtrl', function() {
 
-  // load the controller's module
-  beforeEach(module('angularMeetupApp'));
+  beforeEach(module('angularMeetupApp', 'ControllerTestHelper'));
 
-  var AlbumEditCtrl,
-    scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    AlbumEditCtrl = $controller('AlbumEditCtrl', {
-      $scope: scope
-    });
+  beforeEach(inject(function(jasmineMatchers) {
+    this.addMatchers(jasmineMatchers);
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  var AlbumEditCtrl, scope, controller, rootScope, routeParams, theMocks, fake;
+
+  var createController = function(albumId) {
+    routeParams.albumId = albumId;
+
+    scope = rootScope.$new();
+    AlbumEditCtrl = controller('AlbumEditCtrl', {
+      $scope: scope
+    });
+  };
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function($controller, $rootScope, $routeParams, mocks, fakeResource) {
+    controller = $controller;
+    rootScope = $rootScope;
+    routeParams = $routeParams;
+    theMocks = mocks;
+    fake = fakeResource;
+  }));
+
+  it('should create a new album', function() {
+    fake.album.whenCreate().returns({id: 123});
+
+    // Given that you load the controller for a new item.
+    createController('new');
+
+    // When you save the item.
+    scope.save();
+
+    // Then ensure a new album was created.
+    expect(fake.album).toHaveBeenCreated();
+  });
+
+  it('should update', function() {
+
   });
 });
