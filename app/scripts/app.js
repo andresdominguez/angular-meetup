4ad5ea89f5c831e6ab51c2b2c69dd4fb73d8f02e
andresdom <andresdom@google.com>
2013-05-24 12:05:35 -0400

    formatting

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,35 +4,35 @@ var app = angular.module('angularMeetupApp', ['ngResource']);
 
 app.config(function($routeProvider) {
   $routeProvider
-      .when('/', {
-        templateUrl: 'views/home.html',
-        controller: 'HomeCtrl'
-      })
-      .when('/band-list', {
-        templateUrl: 'views/band-list.html',
-        controller: 'BandListCtrl'
-      })
-      .when('/band-edit/:bandId', {
-        templateUrl: 'views/band-edit.html',
-        controller: 'BandEditCtrl'
-      })
-      .when('/member-list', {
-        templateUrl: 'views/member-list.html',
-        controller: 'MemberListCtrl'
-      })
-      .when('/member-edit/:memberId', {
-        templateUrl: 'views/member-edit.html',
-        controller: 'MemberEditCtrl'
-      })
-      .when('/album-list', {
-        templateUrl: 'views/album-list.html',
-        controller: 'AlbumListCtrl'
-      })
-      .when('/album-edit/:albumId', {
-        templateUrl: 'views/album-edit.html',
-        controller: 'AlbumEditCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+  .when('/', {
+    templateUrl: 'views/home.html',
+    controller: 'HomeCtrl'
+  })
+  .when('/band-list', {
+    templateUrl: 'views/band-list.html',
+    controller: 'BandListCtrl'
+  })
+  .when('/band-edit/:bandId', {
+    templateUrl: 'views/band-edit.html',
+    controller: 'BandEditCtrl'
+  })
+  .when('/member-list', {
+    templateUrl: 'views/member-list.html',
+    controller: 'MemberListCtrl'
+  })
+  .when('/member-edit/:memberId', {
+    templateUrl: 'views/member-edit.html',
+    controller: 'MemberEditCtrl'
+  })
+  .when('/album-list', {
+    templateUrl: 'views/album-list.html',
+    controller: 'AlbumListCtrl'
+  })
+  .when('/album-edit/:albumId', {
+    templateUrl: 'views/album-edit.html',
+    controller: 'AlbumEditCtrl'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
 });
